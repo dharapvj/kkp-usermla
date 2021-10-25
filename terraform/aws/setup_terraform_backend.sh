d8070e7ec31c685a0f5de00432ebe48ecd1090b4
Vijay Dharap <vijay@kubermatic.com>
2021-10-25 10:43:46 +0530

    Fixed s3 bucket name

--- terraform/aws/setup_terraform_backend.sh
+++ terraform/aws/setup_terraform_backend.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-readonly GENERATED_BUCKET_SUFFIX="x_1sux0dwomcxu3w"
+readonly GENERATED_BUCKET_SUFFIX="x1sux0dwomcxu3w"
 readonly TF_STATE_BUCKET_NAME="tf-state-kkp-$GENERATED_BUCKET_SUFFIX"
 
 echo "Setting up S3 bucket ${TF_STATE_BUCKET_NAME} for storing Terraform state."
